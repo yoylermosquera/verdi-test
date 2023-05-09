b22fc20e817a9c3167fe176cb3deeb0dc1692100
DennisH23 <dennis.sierra@antpack.co>
2023-05-09 13:08:54 -0500

    Quotation container created

--- src/components/quotationContainer/index.tsx
+++ src/components/quotationContainer/index.tsx
@@ -1,37 +1,89 @@
-import useMediaQuery from '@/hooks/useMediaQuery';
-import Image from 'next/image';
 import React from 'react';
 
-
 interface FolderTypeContainerProps {
-description?: string;
-dateReceived?: string;
+productName?: string;
+dateQuoteCreated?: string;
+cotizationNumber?: string;
+dateQuoteAccepted?: string;
+productType?: string;
+productDescription?: string;
+userType?: string;
+userName?: string;
 className?: string;
+productCost?: string;
  }
 
 function QuitationContainer({
-description,
-dateReceived,
+productName,
+cotizationNumber,
+dateQuoteCreated,
+dateQuoteAccepted,
+productType,
+productDescription,
+userType,
+userName,
+productCost,
+
+
 className,
 }: FolderTypeContainerProps
 ) {
-// const matches = useMediaQuery('(min-width: 768px)');
-// const iconSize = matches ? 50 : 70;
+
   return (
     <section className={`w-[20.5rem] lg:w-[21.75rem] flex flex-col ${className}`} >
-       <section className='bg-gold-dark border h-[4.5rem] w-full flex flex-row justify-between px-4'>
-            <section className='flex flex-col'>
-                <h1 className='text-title'>Tapetes Hotel Neural</h1>
-                <span>02/04/21</span>
-            </section>
-            <section className='flex flex-col'>
-                <h1 className='text-title bg-white'>#4256</h1>
-                { dateReceived && <span>02/04/21</span>     }
-            </section>
-       </section>
-       <section>
+      <section className='bg-beige border h-[4.5rem] w-full flex flex-row justify-between px-4 py-4'>
+          <section className='flex flex-col gap-1'>
+              {productName && <h1 className='text-button__line '>{productName}</h1>}
+              {dateQuoteCreated && <span className='text-paragraph'>{dateQuoteCreated}</span>}
+          </section>
+          <section className='flex flex-col gap-1'>
+              {cotizationNumber && <h1 className='text-button__line bg-white text-center '>#{cotizationNumber}</h1>}                
+              {dateQuoteAccepted && <span className='text-paragraph'>{dateQuoteAccepted}</span>}
+          </section>
+      </section>
 
-       </section>
+      <section className='flex flex-col gap-5 border px-4 pb-4 pt-5'>
+        <section className='flex flex-row'>
+          <span className='text-title mr-3 my-auto'> Productos:</span>
+          {productType && <span className='text-title my-auto'>{productType}</span>}
+        </section>
+
+        <section className='flex flex-col gap-3 '>
+          <span className='text-title mr-3 my-auto'> Descripción:</span>
+          {productDescription && <span className='text-paragraph text-justify'>{productDescription}</span>}
+        </section>
+
+        <section className='flex flex-row justify-between'>
+          {userType && <span className='text-title my-auto'> {userType}:</span>}
+          {userName && <span className='text-title my-auto'>{userName}</span>}
+        </section>
+
+        {productCost && 
+          <section className='flex flex-row justify-between'>
+            <span className='text-title my-auto'> Precio:</span>
+            <span className='text-title my-auto'>${productCost}</span>
+          </section>
+        }
+
+        <section className='flex flex-row justify-between'>
+          <span 
+          className='text-button__line underline underline-offset-1 hover:cursor-pointer'
+          onClick={() => { }}
+          >
+            Eliminar
+          </span>
+
+          <span
+          className='text-button__line underline underline-offset-1 hover:cursor-pointer'
+          onClick={() => { }}
+          >
+            Ver detalle
+          </span>
+
+        </section>
+
+
+      </section>
     </section>
   );
 }
--- src/pages/folders/index.tsx
+++ src/pages/folders/index.tsx
@@ -3,16 +3,24 @@ import { NextPageWithLayout } from '../_app';
 import { BaseLayout } from '../../components/layouts/BaseLayout';
 import FolderPage from '@/components/folders/folderPage';
 import QuitationContainer from '@/components/quotationContainer';
-import FoldersContainer from '@/components/folders/foldersContainer';
-import NoProductsContainer from '@/components/folders/noProducts';
+
 const  FoldersPage: NextPageWithLayout = () => {
 
   return (
     <div className='h-full flex flex-col grow lg:flex-row lg:justify-center'>
-      {/* <QuitationContainer/> */}
+      <QuitationContainer 
+      productName='Tapetes Hotel Neura'
+      dateQuoteCreated='02/04/21'
+      cotizationNumber='1234'
+      dateQuoteAccepted='02/04/21'
+      productType='Tapete'
+      productDescription={'Tapete tejido con lana de alpaca y bordados de metales finos con acabados en materiales naturales'}
+      userType='Comercial'
+      userName='Juan Perez'
+      productCost='1,000,000'
+      />
       {/* <FolderPage /> */}
-      <NoProductsContainer/>
-      {/* <FoldersContainer/> */}
+      
       
       
     </div>
