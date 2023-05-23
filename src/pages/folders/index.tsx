9a6eeebaf4b94a0de7264001851d1150f084a943
DennisH23 <dennis.sierra@antpack.co>
2023-05-23 11:38:50 -0500

    folders page layout cleaned

--- src/pages/folders/index.tsx
+++ src/pages/folders/index.tsx
@@ -12,7 +12,6 @@ import ProductCard from '@/components/productCard';
 import QuotationPage from '@/components/quotationContainer/quotationPage';
 import QuotationType from '@/components/quotationType';
 import QuotationDetailedPage from '@/components/quotationContainer/quotationDetailedPage';
-import FolderNameModal from '@/components/modals/folderName';
 import FoldersContainer from '@/components/folders/foldersContainer';
 
 
@@ -20,68 +19,7 @@ import FoldersContainer from '@/components/folders/foldersContainer';
 const FoldersPage: NextPageWithLayout = () => {
   return (
     <div className="h-full flex flex-col lg:flex-row lg:justify-center">
-      {/* <QuotationContainer 
-      quotationType='recibida'
-      productName='Tapetes Hotel Neura'
-      dateQuoteCreated='02/04/21'
-      quotationNumber='1234'
-      dateQuoteAccepted='02/0/21'
-      productType='Tapete'
-      productDescription={'Tapete tejido con lana de alpaca y bordados de metales finos con acabados en materiales naturales'}
-      userType='Comercial'
-      userName='Juan Perez'
-      productCost='1,000,000'
-      /> */}
-
-      {/* <QuotationProduct
-        img={productSample}
-        imgTitle='Tapete'
-        productName='Tapete'
-        quotationNumber='1234'
-        productDescription='Tapete de fibras naturales y alambre chino'
-      /> */}
-
-      {/* <UserDetailsContainer
-        userName="Dennis Herrera"
-        dateOfBirth="23 de Febrero de 1998"
-        city="Quito"
-        country="Ecuador"
-        adress="Calle falsa 123"
-        companyName="Hotel Neura"
-        companyUrl="www.hotelneura.com"
-        email="jZlKG@example.com"
-        methodOfknow="Voz a voz"
-        userType="Usuario Directo"
-      /> */}
-
-      {/* <section className='w-[155px] lg:w-[257px]'>
-        <FolderCard
-          img1={productSample}
-          img2={productSample}
-          img3={productSample}
-          title1='Tapete'
-          title2='Tapete'
-          title3='Tapete'
-          folderTitle='Todos mis favoritos'
-          productsAmmount='20'
-        /> 
-      </section>
-       */}
-
-      <FolderPage />
-      
-      {/* <FolderProductsPage/> */}
-      
-      {/* <QuotationPage /> */}
-
-      {/* <QuotationType/> */}
-
-      {/* <QuotationDetailedPage/> */}
-
-       {/* <FolderNameModal/> */}
-       <FoldersContainer/>
-
-
+     
     </div>
   );
 };
